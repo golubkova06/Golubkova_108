fe4c137467d19319b975b59a480cfce5379ed229
Anastasia Golubkova <golubkova009@gmail.com>
2023-01-16 13:41:13 +0200

    add changes

--- Arrays.cpp
+++ Arrays.cpp
@@ -9,19 +9,19 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int arr[20];
+	int arr[15];
 	int i;
 	int n;
 
 	srand(time(NULL));
 
-	for (i = 0;i < 20;i++) {
+	for (i = 0;i < 15;i++) {
 		arr[i] = rand() % 201 - 100;
 		cout << setw(10) << arr[i];
 	}
 	cout << endl;
 	n = 0;
-	for (i = 0;i < 20;i++) {
+	for (i = 0;i < 15;i++) {
 		if (arr[i] < 0) {
 			n++;
 		}
